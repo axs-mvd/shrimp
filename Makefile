c794241d73b88e9ab25720a74548d87ab15fe423
Alexis Rodríguez <axs@montevideo.com.uy>
2025-08-28 23:50:32 -0300

    playing with gun

--- Makefile
+++ Makefile
@@ -5,25 +5,26 @@ PROJECT_VERSION = 0.1.0
 
 BUILD_DEPS = elvis_mk
 
-DEPS =  gun   \
-				sync  \
-				cowboy 
+DEPS = gun    \
+       sync   \
+       cowboy 
 
-dep_sync = git https://github.com/rustyio/sync.git
-dep_sync_commit=master
-RELX_TAR = 0
-
-SHELL_OPTS = -pa ebin \
-             -pa deps/*/ebin \
-             -sname $(PROJECT) \
-						 -s sync \
-             -setcookie $(PROJECT) \
-             -config rel/dev.config \
-             +pc unicode \
-             -run c erlangrc .
-
+dep_sync = git https://github.com/rustyio/sync.git v0.4.1
 dep_elvis_mk = git https://github.com/inaka/elvis.mk.git 1.1.2
 
+dep_gun_commit=2.2.0
+dep_cowboy_commit=2.13.0
+
+RELX_TAR = 0
+
+SHELL_OPTS = -pa ebin               \
+             -pa deps/*/ebin        \
+             -sname $(PROJECT)      \
+             -s sync                \
+             -setcookie $(PROJECT)  \
+             -config rel/dev.config \
+             +pc unicode            \
+             -run c erlangrc .
 
 DEP_PLUGINS = elvis_mk
 
--- src/gun_test.erl
+++ src/gun_test.erl
@@ -1,6 +1,44 @@
 -module(gun_test).
 
+-export([start/0]).
 -export([test/0]).
 
+start() ->
+  application:ensure_all_started(gun).
+
 test() ->
-  hola_que_tal.
+  {ok, ConnPid} = gun:open("example.org", 443),
+  MonitorRef = monitor(process, ConnPid),
+  StreamRef = gun:get(ConnPid, "/"),
+  receive
+    {gun_response, ConnPid, StreamRef, fin, Status, Headers} ->
+      logger:info("Status: ~p", [Status]),
+      logger:info("Headers: ~p", [Headers]),
+      no_data;
+    {gun_response, ConnPid, StreamRef, nofin, Status, Headers} ->
+      logger:info("Status: ~p", [Status]),
+      logger:info("Headers: ~p", [Headers]),
+      receive_data(ConnPid, MonitorRef, StreamRef);
+    {'DOWN', MonitorRef, process, ConnPid, Reason} ->
+      error_logger:error_msg("Oops!"),
+      exit(Reason)
+  after 1000 ->
+    exit(timeout)
+  end.
+
+receive_data(ConnPid, MonitorRef, StreamRef) ->
+  receive
+    {gun_data, ConnPid, StreamRef, nofin, Data} ->
+      io:format("~s~n", [Data]),
+      receive_data(ConnPid, MonitorRef, StreamRef);
+    {gun_data, ConnPid, StreamRef, fin, Data} ->
+      io:format("~s~n", [Data]);
+    {'DOWN', MonitorRef, process, ConnPid, Reason} ->
+      logger:error("Oops!"),
+      exit(Reason)
+  after 1000 ->
+    logger:info("closing connection"),
+    demonitor(MonitorRef),
+    gun:close(ConnPid),
+    exit(timeout)
+  end.
